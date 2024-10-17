d5aa2a98cec9c3a9cf4591b709b8ea4ebdfa8846
allisonrosej <70971185+allisonrosej@users.noreply.github.com>
2024-10-17 15:36:24 -0700

    Add files via upload

--- week0/listener.py
+++ week0/listener.py
@@ -37,24 +37,22 @@
 ## to the 'chatter' topic
 
 import rospy
-from std_msgs.msg import String
+#from std_msgs.msg import String
+from week0.msg   import week0msg
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+   
+    rospy.loginfo(rospy.get_caller_id() + 'I heard: %s ' , data)
 
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+   
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber('chatter', String, callback)
+    rospy.Subscriber('chatter', week0msg, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
+    # spin() keeps Python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
     listener()
+
